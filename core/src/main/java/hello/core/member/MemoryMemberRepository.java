8b5a9228ca9ce24307b8bf595dab582bc58bf006
Wrinkk <115036933+Wrinkk@users.noreply.github.com>
2024-01-02 23:46:10 +0900

    회원 도메인, 주문과 할인 예제

---
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>(); // 원래는 동시성 이슈때문에 concurrent 를 사용
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
