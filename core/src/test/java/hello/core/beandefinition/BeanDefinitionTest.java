055018cdf4b62615f2fa099e0c5160bef283fb56
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-01-11 23:55:44 +0900

    beanDefinition 예제

--- core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
+    /* 스프링이 다양한 형태의 설정 정보를 BeanDefinition으로 추상화 해서 사용한다.*/
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
+//        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
     @Test
     @DisplayName("빈 설정 메타정보 확인")
     void findApplicationBean() {
