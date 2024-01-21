32560fe3602a3cfac5899163868caf9809f408a2
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-01-21 23:54:08 +0900

    옵션 처리 예제

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -13,15 +13,17 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", // hello.core의 멤버 하위파일들만 컴포넌트 됨.
-        basePackageClasses = AutoAppConfig.class,
+//        basePackages = "hello.core.member", // hello.core의 멤버 하위파일들만 컴포넌트 됨.
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 스프링 빈을 자동으로 끌어올려주는 것 (동기화?를 말하는것같다) 기존예제 코드를 살리기위해서 excludeFilters 넣어줌.
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
+/*    @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+*/
+
 // 생성자가 많아도, 타입을 찾아 자동으로 주입을 함.
 }
