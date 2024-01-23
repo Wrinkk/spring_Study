248f2cb19cb3c841a20031003b48fbdea481fffe
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-01-23 23:47:50 +0900

    롬복과 생성자 주입 선택

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,23 +3,28 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // 의존관계 자동 주입은 스프링 컨테이너가 관리하는 스프링 빈이어야 동작한다.
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 // @Autowired private MemberRepository memberRepository;
 // @Autowired private DiscountPolicy discountPolicy;
     // 외부에서 변경이 불가능해서 테스트 하기 힘듬. (필드 주입), DI 프레임워크가 없으면 아무것도 할 수 없음.
 
-    private  MemberRepository memberRepository;
-    private  DiscountPolicy discountPolicy;
+    private  final MemberRepository memberRepository;
+    private  final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+
+
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } // lombok이 알아서 생성자 만들어줌. 
+
 //    @Autowired(required = false) //선택적일 때, (필수가 아닐 때)
 //    public void setMemberRepository(MemberRepository memberRepository) {
 //        this.memberRepository = memberRepository;
@@ -41,11 +46,11 @@ public class OrderServiceImpl implements OrderService {
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); 둘다 의존성 DIP를 지키지않았음
 
-    @Autowired // 일반 메서드 주입, 한번에 여러필드를 주입 받을 수 있다. (잘사용안함)
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired // 일반 메서드 주입, 한번에 여러필드를 주입 받을 수 있다. (잘사용안함)
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
