71534ab4d2dd4b0ed0868dbd4471e9d572f2dc52
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-02-07 23:54:37 +0900

    프로토타입 스코프
    싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- core/src/test/java/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/scope/SingletonWithPrototypeTest1.java
@@ -2,14 +2,17 @@ package scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/* 매번 사용할 때마다 의존관계 주입이 완료된 새로운 객체가 필요하면 사용 (프로토타입 빈 사용할 때) */
 public class SingletonWithPrototypeTest1 {
 
     @Test
@@ -21,7 +24,7 @@ public class SingletonWithPrototypeTest1 {
 
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(2);
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
 
     }
 
@@ -35,12 +38,18 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
         private final PrototypeBean prototypeBean; //생성 시점에 주입
+
+        /* ObjectFactory 상속,옵션,스트림 처리 등 편의 기능 많음, 별도의 라이브러리 필요 없음, 스프링에 의존 */
+        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider; // 별도의 라이브러리 필요, get() 메소드 하나로 기능 단순,
+        // 자바 표준이므로 스프링이 아닌 컨테이너도 사용할 수 있다.
 
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
@@ -48,6 +57,8 @@ public class SingletonWithPrototypeTest1 {
         }
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
