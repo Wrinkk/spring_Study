fb0600bff7571b1105e8b220a1080b6745e7ccaf
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-02-11 23:55:42 +0900

    servlet 예제

---
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,17 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+// 서블릿은 톰캣 같은 WAS를 직접 설치하고 그 위에 서블릿 코드를 클래스 파일로 빌드해서 올린 다음, 톰캣 서버를 실행하면 됨.(복잡)
+// 스프링 부트는 톰캣 서버를 내장하고 있으므로, 톰캣 서버 설치 없이 편리하게 서블릿 코드를 실행 가능
+@ServletComponentScan // 서블릿 자동 등록
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
