022ed195ff8dc7a964210cea78c1301337f88174
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-03-02 23:53:56 +0900

    MVC 프레임워크 예제

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -2,6 +2,7 @@ package hello.servlet.web.frontcontroller.v1.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-public class MemberListControllerV1 implements ControllerV1{
+public class MemberListControllerV1 implements ControllerV1 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
     @Override
--- servlet/src/main/webapp/WEB-INF/views/new-form.jsp
+++ servlet/src/main/webapp/WEB-INF/views/new-form.jsp
@@ -5,7 +5,7 @@
     <title>Title</title>
 </head>
 <body>
-<!-- 상대경로 사용, [현재 URL이 속한 계층 경로 + /save] -->
+<!-- 상대경로 사용, [현재 URL이 속한 계층 경로 + /save] , 실무에서는 절대경로를 사용하는것을 적극. -->
 <form action="save" method="post">
     username: <input type="text" name="username" />
     age: <input type="text" name="age" />
