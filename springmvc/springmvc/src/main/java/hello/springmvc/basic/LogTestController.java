95b75adbdb26500bd11b8b9def31ffdfc9b0de5a
DESKTOP-BABBNFL\wj830 <wjdxodndjaak@naver.com>
2024-04-03 22:59:57 +0900

    로깅 알아보기 debug(트레이스만 빼고 봄,개발 서버), trace(로그다봄), info(운영서버,기본값), warn ,error 롬복의 SLF4J ,  로그를 사용하면 시스템아웃보다 좋음(내부 버퍼링, 멀티쓰레드 등등)

--- springmvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,10 +1,12 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Slf4j
 @RestController
 public class LogTestController {
 
@@ -15,7 +17,13 @@ public class LogTestController {
         String name = "Spring";
 
         System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
         log.info(" info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
 
         return "ok";
     }
